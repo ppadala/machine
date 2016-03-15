3a23e7a9412e7324cbfd1bb1c54ecca9bfd4fac5
Pradeep Padala <pradeep@containerx.io>
2016-03-15 15:14:35 -0700

    Reverting azure.go to master

--- drivers/azure/azure.go
+++ drivers/azure/azure.go
@@ -1,20 +1,8 @@
 package azure
 
 import (
-	"crypto/sha1"
-	"encoding/base64"
-	"encoding/pem"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"strings"
-
-	"github.com/Azure/azure-sdk-for-go/management"
-	"github.com/Azure/azure-sdk-for-go/management/hostedservice"
-	"github.com/Azure/azure-sdk-for-go/management/virtualmachine"
-	"github.com/Azure/azure-sdk-for-go/management/vmutils"
 	"net"
 	"net/url"
 
@@ -70,30 +58,6 @@ const (
 // Driver represents Azure Docker Machine Driver.
 type Driver struct {
 	*drivers.BaseDriver
-<<<<<<< HEAD
-	SubscriptionID          string
-	SubscriptionCert        string
-	PublishSettingsFilePath string
-	Location                string
-	Size                    string
-	UserPassword            string
-	Image                   string
-	OS                      string
-	DockerPort              int
-	DockerSwarmMasterPort   int
-}
-
-const (
-	defaultWindowsImage    = "cx-win-2016-2"
-	defaultLinuxImage      = "b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-15_10-amd64-server-20151116.1-en-us-30GB"
-	defaultDockerPort      = 2376
-	defaultSwarmMasterPort = 3376
-	defaultLocation        = "West US"
-	defaultSize            = "Small"
-	defaultSSHPort         = 22
-	defaultSSHUsername     = "ubuntu"
-)
-=======
 
 	Environment    string
 	SubscriptionID string
@@ -134,7 +98,6 @@ func NewDriver(hostName, storePath string) drivers.Driver {
 	}
 	return d
 }
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 
 // GetCreateFlags returns list of create flags driver accepts.
 func (d *Driver) GetCreateFlags() []mcnflag.Flag {
@@ -214,27 +177,6 @@ func (d *Driver) GetCreateFlags() []mcnflag.Flag {
 			Name:  flAzurePrivateIPAddr,
 			Usage: "Specify a static private IP address for the machine",
 		},
-<<<<<<< HEAD
-		mcnflag.StringFlag{
-			Name:  "azure-os",
-			Usage: "OS for the Azure VM (Windows|Linux)",
-			Value: drivers.LINUX,
-		},
-	}
-}
-
-func NewDriver(hostName, storePath string) drivers.Driver {
-	d := &Driver{
-		DockerPort:            defaultDockerPort,
-		DockerSwarmMasterPort: defaultSwarmMasterPort,
-		Location:              defaultLocation,
-		Size:                  defaultSize,
-		BaseDriver: &drivers.BaseDriver{
-			SSHPort:     defaultSSHPort,
-			SSHUser:     defaultSSHUsername,
-			MachineName: hostName,
-			StorePath:   storePath,
-=======
 		mcnflag.BoolFlag{
 			Name:  flAzureUsePrivateIP,
 			Usage: "Use private IP address of the machine to connect",
@@ -250,7 +192,6 @@ func NewDriver(hostName, storePath string) drivers.Driver {
 		mcnflag.StringSliceFlag{
 			Name:  flAzurePorts,
 			Usage: "Make the specified port number accessible from the Internet",
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 		},
 	}
 }
@@ -284,29 +225,6 @@ func (d *Driver) SetConfigFromFlags(fl drivers.DriverOptions) error {
 		}
 	}
 
-<<<<<<< HEAD
-	return d.SSHUser
-}
-
-func (d *Driver) GetOS() string {
-	return d.OS
-}
-
-// DriverName returns the name of the driver
-func (d *Driver) DriverName() string {
-	return "azure"
-}
-
-func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
-	d.SubscriptionID = flags.String("azure-subscription-id")
-
-	cert := flags.String("azure-subscription-cert")
-	publishSettings := flags.String("azure-publish-settings-file")
-	image := flags.String("azure-image")
-	username := flags.String("azure-username")
-	OS := flags.String("azure-os")
-	d.OS = strings.ToLower(OS)
-=======
 	// Optional flags or Flags of other types
 	d.Environment = fl.String(flAzureEnvironment)
 	d.OpenPorts = fl.StringSlice(flAzurePorts)
@@ -319,7 +237,6 @@ func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
 	// Set flags on the BaseDriver
 	d.BaseDriver.SSHPort = sshPort
 	d.SetSwarmConfigFromFlags(fl)
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 
 	log.Debug("Set configuration from flags.")
 	return nil
@@ -335,23 +252,12 @@ func (d *Driver) PreCreateCheck() (err error) {
 		return err
 	}
 
-<<<<<<< HEAD
-	if d.OS == drivers.WINDOWS {
-		image = defaultWindowsImage
-	}
-
-	if image == "" {
-		d.Image = defaultLinuxImage
-	} else {
-		d.Image = image
-=======
 	// Register used resource providers with current Azure subscription.
 	if err := c.RegisterResourceProviders(
 		"Microsoft.Compute",
 		"Microsoft.Network",
 		"Microsoft.Storage"); err != nil {
 		return err
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 	}
 
 	// Validate if firewall rules can be read correctly
@@ -390,210 +296,6 @@ func (d *Driver) Create() error {
 		return err
 	}
 
-<<<<<<< HEAD
-	client, err := d.getClient()
-	if err != nil {
-		return err
-	}
-
-	// check azure DNS to make sure name is available
-	_, err = hostedservice.NewClient(client).CheckHostedServiceNameAvailability(d.MachineName)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-//
-// getServiceCertFingerprint returns the thumbprint of the certificate in a
-// file
-//
-// Parameters:
-//   certPath: path to the cert file
-// Returns:
-//   string: thumbprint string
-//   error: error from reading the file or decoding cert
-//
-func getServiceCertFingerprint(certPath string) (string, error) {
-	certData, readErr := ioutil.ReadFile(certPath)
-	if readErr != nil {
-		return "", readErr
-	}
-
-	block, rest := pem.Decode(certData)
-	if block == nil {
-		return "", errors.New(string(rest))
-	}
-
-	sha1sum := sha1.Sum(block.Bytes)
-	fingerprint := fmt.Sprintf("%X", sha1sum)
-	return fingerprint, nil
-}
-
-//
-// configureForLinux sets up the VM role for Linux specific configuration
-//
-// Paramters:
-//   role: role that needs to be updated with Linux configuration
-//   dnsName: name of the machine that we are trying to create
-//
-// Returns:
-//   error: errors from reading certs, getting thumbprint and adding
-//   certificate to hostedservice
-//
-func (d *Driver) configureForLinux(role *virtualmachine.Role, dnsName string) error {
-	// Get the Azure client
-	client, err := d.getClient()
-	if err != nil {
-		return err
-	}
-
-	// Setup the image configuration
-	vmutils.ConfigureDeploymentFromPlatformImage(
-		role,
-		d.Image,
-		// XXX - need to query storage service to find the right
-		// storage backend based on the location
-		fmt.Sprintf("http://azurevmpp1.blob.core.windows.net/vhds/%s.vhd", dnsName),
-		"")
-
-	// Read the certificate
-	data, err := ioutil.ReadFile(d.azureCertPath())
-	if err != nil {
-		return err
-	}
-
-	// Add the certificate to the hostedservice
-	if _, err := hostedservice.NewClient(client).AddCertificate(dnsName, data, "pfx", ""); err != nil {
-		return err
-	}
-
-	thumbPrint, err := getServiceCertFingerprint(d.azureCertPath())
-	if err != nil {
-		return err
-	}
-
-	vmutils.ConfigureForLinux(role, dnsName, d.SSHUser, d.UserPassword, thumbPrint)
-	vmutils.ConfigureWithPublicSSH(role)
-
-	role.UseCertAuth = true
-	role.CertPath = d.azureCertPath()
-	return nil
-}
-
-//
-// configureForWindows sets up the VM role for Windows specific configuration
-//
-// Paramters:
-//   role: role that needs to be updated with Windows configuration
-//   dnsName: name of the machine that we are trying to create
-//
-// Returns:
-//   None
-//
-func (d *Driver) configureForWindows(role *virtualmachine.Role, dnsName string) {
-	vmutils.ConfigureDeploymentFromUserVMImage(
-		role,
-		d.Image)
-	vmutils.ConfigureForWindows(role, dnsName, d.SSHUser, d.UserPassword, true, "")
-	vmutils.ConfigureWithPublicSSH(role)
-	vmutils.ConfigureWithPublicRDP(role)
-	vmutils.ConfigureWithPublicPowerShell(role)
-	vmutils.ConfigureWithExternalPort(role, "WinRMu", 5985, 5985,
-		virtualmachine.InputEndpointProtocolTCP)
-}
-
-func (d *Driver) Create() error {
-	if err := d.setUserSubscription(); err != nil {
-		return err
-	}
-
-	log.Info("Creating Azure machine...")
-	client, err := d.getClient()
-	if err != nil {
-		return err
-	}
-
-	dnsName := d.MachineName
-
-	// 1. Create the hosted service
-	if err := hostedservice.NewClient(client).CreateHostedService(hostedservice.CreateHostedServiceParameters{
-		ServiceName: dnsName,
-		Location:    d.Location,
-		Label:       base64.StdEncoding.EncodeToString([]byte(dnsName))}); err != nil {
-		return err
-	}
-
-	// 2. Generate certificates
-	log.Debug("Generating certificate for Azure...")
-	if err := d.generateCertForAzure(); err != nil {
-		return err
-	}
-
-	// 3. Setup VM configuration for creation
-	log.Debug("Setting up VM configuration...")
-	var operationID management.OperationID
-
-	role := vmutils.NewVMConfiguration(dnsName, d.Size)
-	if d.OS == drivers.LINUX {
-		err := d.configureForLinux(&role, dnsName)
-		if err != nil {
-			goto fail
-		}
-
-	} else {
-		d.configureForWindows(&role, dnsName)
-	}
-
-	log.Debug("Authorizing docker ports...")
-	if err := d.addDockerEndpoints(&role); err != nil {
-		goto fail
-	}
-
-	// 4. Create the VM
-	operationID, err = virtualmachine.NewClient(client).
-		CreateDeployment(role, dnsName, virtualmachine.CreateDeploymentOptions{})
-	if err != nil {
-		goto fail
-	}
-
-	// 5. Wait for operation
-	err = client.WaitForOperation(operationID, nil)
-	if err != nil {
-		goto fail
-	}
-	goto success
-
-fail:
-	hostedservice.NewClient(client).DeleteHostedService(dnsName, true)
-	return err
-
-success:
-	return nil
-}
-
-//
-// getClient returns a client for Azure API endpoint. Requires the
-// publishsettings file to be already specified to the driver
-//
-// Parameters:
-//   None
-// Returns:
-//   management.Client: client object to Azure API endpoint
-//   error: errors from establishing new client
-//
-func (d *Driver) getClient() (management.Client, error) {
-	client, err := management.ClientFromPublishSettingsFile(d.PublishSettingsFilePath, "")
-	if err != nil {
-		return management.NewAnonymousClient(), err
-	}
-	return client, nil
-}
-
-func (d *Driver) GetURL() (string, error) {
-	if err := drivers.MustBeRunning(d); err != nil {
-=======
 	if err := c.CreateResourceGroup(d.ResourceGroup, d.Location); err != nil {
 		return err
 	}
@@ -678,7 +380,6 @@ func (d *Driver) Remove() error {
 // GetIP returns public IP address or hostname of the machine instance.
 func (d *Driver) GetIP() (string, error) {
 	if err := d.checkLegacyDriver(true); err != nil {
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 		return "", err
 	}
 
@@ -705,19 +406,10 @@ func (d *Driver) GetURL() (string, error) {
 		return "", err
 	}
 
-<<<<<<< HEAD
-	client, err := d.getClient()
-	if err != nil {
-		return state.Error, err
-	}
-
-	dockerVM, err := virtualmachine.NewClient(client).GetDeployment(d.MachineName, d.MachineName)
-=======
 	// NOTE (ahmetalpbalkan) I noticed that this is not used until machine is
 	// actually created and provisioned. By then GetIP() should be returning
 	// a non-empty IP address as the VM is already allocated and connected to.
 	ip, err := d.GetIP()
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 	if err != nil {
 		return "", err
 	}
@@ -735,16 +427,6 @@ func (d *Driver) GetState() (state.State, error) {
 		return state.None, err
 	}
 
-<<<<<<< HEAD
-	vmState := dockerVM.RoleInstanceList[0].PowerState
-	switch vmState {
-	case "Started":
-		return state.Running, nil
-	case "Starting":
-		return state.Starting, nil
-	case "Stopped":
-		return state.Stopped, nil
-=======
 	c, err := d.newAzureClient()
 	if err != nil {
 		return state.None, err
@@ -753,7 +435,6 @@ func (d *Driver) GetState() (state.State, error) {
 		d.ResourceGroup, d.naming().VM())
 	if err != nil {
 		return state.None, err
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 	}
 
 	machineState := machineStateForVMPowerState(powerState)
@@ -768,25 +449,11 @@ func (d *Driver) Start() error {
 		return err
 	}
 
-<<<<<<< HEAD
-	client, err := d.getClient()
-	if err != nil {
-		return err
-	}
-
-	if _, err := virtualmachine.NewClient(client).StartRole(d.MachineName, d.MachineName, d.MachineName); err != nil {
-		return err
-	}
-
-	d.IPAddress, err = d.GetIP()
-	return err
-=======
 	c, err := d.newAzureClient()
 	if err != nil {
 		return err
 	}
 	return c.StartVirtualMachine(d.ResourceGroup, d.naming().VM())
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 }
 
 // Stop issues a power off for the virtual machine instance.
@@ -795,17 +462,8 @@ func (d *Driver) Stop() error {
 		return err
 	}
 
-<<<<<<< HEAD
-	client, err := d.getClient()
-	if err != nil {
-		return err
-	}
-
-	if _, err := virtualmachine.NewClient(client).ShutdownRole(d.MachineName, d.MachineName, d.MachineName); err != nil {
-=======
 	c, err := d.newAzureClient()
 	if err != nil {
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 		return err
 	}
 	log.Info("NOTICE: Stopping an Azure Virtual Machine is just going to power it off, not deallocate.")
@@ -819,19 +477,6 @@ func (d *Driver) Restart() error {
 		return err
 	}
 
-<<<<<<< HEAD
-	client, err := d.getClient()
-	if err != nil {
-		return err
-	}
-
-	if _, err := virtualmachine.NewClient(client).RestartRole(d.MachineName, d.MachineName, d.MachineName); err != nil {
-		return err
-	}
-
-	d.IPAddress, err = d.GetIP()
-	return err
-=======
 	// NOTE(ahmetalpbalkan) Azure will always keep the VM in Running state
 	// during the restart operation. Hence we rely on returned async operation
 	// polling to make sure the reboot is waited upon.
@@ -840,7 +485,6 @@ func (d *Driver) Restart() error {
 		return err
 	}
 	return c.RestartVirtualMachine(d.ResourceGroup, d.naming().VM())
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 }
 
 // Kill stops the virtual machine role instance.
@@ -851,61 +495,6 @@ func (d *Driver) Kill() error {
 	return d.Stop()
 }
 
-<<<<<<< HEAD
-func (d *Driver) Remove() error {
-	if err := d.setUserSubscription(); err != nil {
-		return err
-	}
-
-	client, err := d.getClient()
-	if err != nil {
-		return err
-	}
-
-	hostedClient := hostedservice.NewClient(client)
-	if _, err := hostedClient.CheckHostedServiceNameAvailability(d.MachineName); err != nil {
-		return err
-	}
-
-	_, err = hostedClient.DeleteHostedService(d.MachineName, true)
-	return err
-}
-
-func (d *Driver) setUserSubscription() error {
-	if d.PublishSettingsFilePath != "" {
-		return ImportPublishSettingsFile(d.PublishSettingsFilePath)
-	}
-	return ImportPublishSettings(d.SubscriptionID, d.SubscriptionCert)
-}
-
-func (d *Driver) addDockerEndpoints(vmConfig *virtualmachine.Role) error {
-	configSets := vmConfig.ConfigurationSets
-	if len(configSets) == 0 {
-		return errors.New("no configuration set")
-	}
-	for i := 0; i < len(configSets); i++ {
-		if configSets[i].ConfigurationSetType != "NetworkConfiguration" {
-			continue
-		}
-		ep := virtualmachine.InputEndpoint{
-			Name:      "docker",
-			Protocol:  "tcp",
-			Port:      d.DockerPort,
-			LocalPort: d.DockerPort,
-		}
-		if d.SwarmMaster {
-			swarmEp := virtualmachine.InputEndpoint{
-				Name:      "docker swarm",
-				Protocol:  "tcp",
-				Port:      d.DockerSwarmMasterPort,
-				LocalPort: d.DockerSwarmMasterPort,
-			}
-			configSets[i].InputEndpoints = append(configSets[i].InputEndpoints, swarmEp)
-			log.Debugf("added Docker swarm master endpoint (port %d) to configuration", d.DockerSwarmMasterPort)
-		}
-		configSets[i].InputEndpoints = append(configSets[i].InputEndpoints, ep)
-		log.Debugf("added Docker endpoint (port %d) to configuration", d.DockerPort)
-=======
 // checkLegacyDriver errors out if it encounters an Azure VM created with the
 // legacy (<=0.6.0) docker-machine Azure driver.
 func (d *Driver) checkLegacyDriver(short bool) error {
@@ -914,7 +503,6 @@ func (d *Driver) checkLegacyDriver(short bool) error {
 			return errors.New("New azure driver cannot manage old VMs, downgrade to v0.6.0")
 		}
 		return errors.New("New azure driver uses the new Azure Resource Manager APIs and therefore cannot manage this existing machine created with old azure driver. Please downgrade to docker-machine 0.6.0 to continue using these machines or to remove them.")
->>>>>>> 25672016fafa6824b0b386e5d83ca7ffa0e5440e
 	}
 	return nil
 }
